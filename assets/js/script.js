322073d608f716a7a3cd033ade2d86f0c9272dc1
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-04 23:03:58 -0600

    add id to button for js to call on, add eventListener to button use querySelector for button id and make that a variable

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log(window.document);
+
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+// THIS JUST MADE THE NAME OF THE BUTTON ELEMENT E6^^
+buttonEl.addEventListener("click", function() {
+  alert("button clicked");
+});
+
